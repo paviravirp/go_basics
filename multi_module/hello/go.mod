13732aa5ed1a44ac3ee9ee9cb436b9a7cdc25616
paviravirp <pavi.ravirp21@gmail.com>
2022-10-28 12:42:05 -0400

    Added multi module go example

--- multi_module/hello/go.mod
+++ multi_module/hello/go.mod
@@ -2,4 +2,6 @@ module hello
 
 go 1.19
 
-replace multi_module/input => ./input
+replace multi_module/input => ../input
+
+require multi_module/input v0.0.0-00010101000000-000000000000
--- multi_module/hello/hello.go
+++ multi_module/hello/hello.go
@@ -1,11 +1,10 @@
 package main
 
-import {
-	"fmt"
-	"multi_module/input"
-}
+import "fmt"
+import "multi_module/input"
+
 
 func main() {
-	message := input.Hello("Jane");
+	message := input.Input("Jane");
 	fmt.Println(message);
 }
